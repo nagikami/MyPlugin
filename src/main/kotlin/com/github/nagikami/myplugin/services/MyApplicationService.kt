0178566fd1a94512fd90adba42e849a23f366ceb
GitHub Action <action@github.com>
2022-03-28 01:47:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/nagikami/myplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nagikami/myplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nagikami.myplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nagikami.myplugin.MyBundle
 
 class MyApplicationService {
 
